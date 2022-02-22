07341b7833c40f225cfb0e5a74454c5486801dbf
ILYASSE1 <96141502+ILYASSE1@users.noreply.github.com>
2022-02-22 20:37:11 +0100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+clear
+#!/bin/bash
+	choco install figlet
+	figlet Tool-All | lolcat --animate
+echo -e " \e[92m Start "
+	choco install wget
+choco upgrade
+choco install lolcat
+choco install curl
+choco install openssh
+choco install unzip
+choco install python2
+choco install python3
+choco install php
+echo "done"
